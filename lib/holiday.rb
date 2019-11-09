85984aa96e090c35c9ddf70d0a5a18f6fe2b583d
Burbage Stacey <rstacey06@gmail.com>
2019-11-09 16:16:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
       puts"  #{holiday.to_s.split('_').map 
-      {|x|x.capitalize }.join(' ') }: #{supplies.join(", ")}"
+      {|w|w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end
   end
 
